54e32552c4b99d316cec61fa5e8299084fd19909
mdrdani <sudokill3004@gmail.com>
2020-03-07 20:25:19 +0700

    Vuetify, Vue Router and Initial Components

---
+++ resources/assets/js/components/Toolbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-card
+    color="grey lighten-4"
+    flat
+    height="100px"
+    tile
+  >
+    <v-toolbar dense>
+      <!--<v-app-bar-nav-icon></v-app-bar-nav-icon>-->
+
+      <v-toolbar-title>ChitChat</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+     <v-toolbar-items class="hidden-sm-and-down">
+         <v-btn flat>Forum</v-btn>
+         <v-btn flat>Pertanyaan</v-btn>
+         <v-btn flat>Kategori</v-btn>
+         <v-btn flat>Login</v-btn>
+     </v-toolbar-items>
+    </v-toolbar>
+    </v-card>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="">
+    
+</style>
